59a3c279d4239bc70836a15491e23ee1157e10b9
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-06 13:05:13 -0700

    Add more column to the price table

--- coinfolio-project/src/components/Ticker.jsx
+++ coinfolio-project/src/components/Ticker.jsx
@@ -3,12 +3,14 @@
 function Ticker({ coin, index }) {
   return (
     <tr className="divide-solid">
+      <td className="border-t border-b border-gray-600 px-4 py-2">{coin.id}</td>
       <td className="border-t border-b border-gray-600 px-4 py-2">{coin.name}</td>
       <td className="border-t border-b border-gray-600 px-4 py-2">{coin.symbol}</td>
       <td className="border-t border-b border-gray-600 px-4 py-2">${coin.quote.USD.price.toFixed(2)}</td>
-      <td className="border-t border-b border-gray-600 px-4 py-2">{coin.change1h}</td>
+      <td className="border-t border-b border-gray-600 px-4 py-2">{coin.quote.USD.percent_change_1h.toFixed(2)}%</td>
       <td className="border-t border-b border-gray-600 px-4 py-2">{coin.quote.USD.percent_change_24h.toFixed(2)}%</td>
-      <td className="border-t border-b border-gray-600 px-4 py-2">{coin.change7d}</td>
+      <td className="border-t border-b border-gray-600 px-4 py-2">{coin.quote.USD.percent_change_7d.toFixed(2)}%</td>
+      <td className="border-t border-b border-gray-600 px-4 py-2">{coin.quote.USD.market_cap_dominance.toFixed(1)}%</td>
     </tr>
   );
 }
