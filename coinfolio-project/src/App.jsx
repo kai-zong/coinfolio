45f514fceea176b7e4bea3567c0f63bd326239b4
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-05 22:24:03 -0700

    test app can correctly get data

--- coinfolio-project/src/App.jsx
+++ coinfolio-project/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import fakeData from './fakeData';
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+  console.log(fakeData[0])
 
   return (
     <h1 className="text-3xl font-bold underline">
