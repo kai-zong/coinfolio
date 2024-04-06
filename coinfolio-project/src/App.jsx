aa7f5852e5e36fd693cb46f846b90b6bbd9c206b
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-05 22:51:43 -0700

    Create basic home page layout with each asset price ticker

--- coinfolio-project/src/App.jsx
+++ coinfolio-project/src/App.jsx
@@ -1,17 +1,32 @@
 import { useState } from 'react';
 import './App.css';
 import fakeData from './fakeData';
+import Ticker from './components/Ticker';
 
+console.log(fakeData[0])
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  console.log(fakeData[0])
+  const [coins, setCoins] = useState(fakeData)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <nav className="flex justify-evenly">
+        <h1 className='text-center text-3xl font-bold m-5'>Coinfolio</h1>
+
+        <ul className='flex items-center justify-evenly'>
+          <li className='m-5'>Home</li>
+          <li className='m-5'>Portfolio</li>
+          <li className='m-5'>Transactions</li>
+        </ul>
+      </nav>
+
+      {/* here shows the content all visitor can see, price tickers */}
+      <div className='flex flex-col justify-evenly'>
+        {coins.map((coin, index) => (
+          <Ticker key={index} coin={coin} />
+        ))}
+      </div>
+    </>
   )
 }
 
