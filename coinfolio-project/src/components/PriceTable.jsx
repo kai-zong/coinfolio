075af6c91828d075a9147f3fc36b3a4604227e69
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-06 13:05:13 -0700

    Add more column to the price table

---
+++ coinfolio-project/src/components/PriceTable.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Ticker from './Ticker';
+
+function PriceTable({ coins }) {
+    return (
+        <div className='w-full'>
+            <table className="table-auto w-full text-left">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2">#</th>
+                        <th className="px-4 py-2">Name</th>
+                        <th className="px-4 py-2">Symbol</th>
+                        <th className="px-4 py-2">Price</th>
+                        <th className="px-4 py-2">1h %</th>
+                        <th className="px-4 py-2">1d %</th>
+                        <th className="px-4 py-2">7d %</th>
+                        <th className="px-4 py-2">Dominance %</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {coins.map((coin, index) => (
+                        <Ticker key={index} coin={coin} />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default PriceTable;
