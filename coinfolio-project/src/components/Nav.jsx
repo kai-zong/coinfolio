20816f08c4fc11aec2a4330bf3c20341603ef9c5
kai-zong <zong.k@northeastern.edu>
2024-04-07 15:47:07 -0700

    Fix: Added three states in the context, one for user data, one for the public coin list, and one for the filtered public coin list.
    Feat: Added search function for the public coin list

--- coinfolio-project/src/components/Nav.jsx
+++ coinfolio-project/src/components/Nav.jsx
@@ -1,53 +1,87 @@
-import React, {useState, useEffect} from 'react';
-import { useMediaQuery } from 'react-responsive';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+import { useNavigate } from "react-router-dom";
+import { useUserAndPriceTable } from "../UserAndPriceTableContext";
 
 function Nav() {
-    const isSmallIcon = useMediaQuery({ query: '(max-width: 700px)' });
-    const isSmallSearch = useMediaQuery({ query: '(max-width: 600px)' });
-    const [signingButtonText, setSigningButtonText] = useState("Sign In");
-    let isLoggedIn = true;
+  const isSmallIcon = useMediaQuery({ query: "(max-width: 700px)" });
+  const isSmallSearch = useMediaQuery({ query: "(max-width: 600px)" });
+  const [signingButtonText, setSigningButtonText] = useState("Sign In");
+  const {coins, setDisplayedCoins} = useUserAndPriceTable();
+  let isLoggedIn = true;
 
-    useEffect (() => {
-        if (isLoggedIn){
-            setSigningButtonText("Sign Out");
-        }
-        else{
-            setSigningButtonText("Sign In");
-        }
-    }, [isLoggedIn]);
-
-    let navigate = useNavigate();
-    function navigateBasedOnLogging(isLoggedIn) {
-        if (isLoggedIn) {
-            navigate('/portfolio');
-        } else {
-            navigate('/signin');
-        }
+  useEffect(() => {
+    if (isLoggedIn) {
+      setSigningButtonText("Sign Out");
+    } else {
+      setSigningButtonText("Sign In");
     }
-    return (
-        <nav className="flex items-center justify-between w-full border-b border-gray-700" >
-            <div className="flex items-center justify-start pl-3">
-                <img className="h-10 w-auto" src="/src/assets/logo/png/Small-logo-color.png" alt="Small Logo" />
-                {!isSmallIcon && (
-                    <h1 className="text-2xl font-bold pl-3">Coinfolio</h1>
-                )}
-            </div>
-            
-            <div className="flex items-center gap-2">
-                {!isSmallSearch && (
-                    <div className="search-bar">
-                        <input className="px-3 py-2 border border-gray-300 rounded-md" type="text" placeholder="Search" />
-                    </div>
-                )}
-                <div className="buttons pr-1">
-                    <button className="px-3 py-2 m-1 bg-blue-700 text-white rounded-md" onClick={()=>navigateBasedOnLogging(isLoggedIn)}>Portfolio</button>
-                    <button className="px-3 py-2 m-1 bg-blue-700 text-white rounded-md">{signingButtonText}</button>
-                </div>
-            </div>
-        </nav >
-    );
-};
+  }, [isLoggedIn]);
+
+  let navigate = useNavigate();
+  function navigateBasedOnLogging(isLoggedIn) {
+    if (isLoggedIn) {
+      navigate("/portfolio");
+    } else {
+      navigate("/signin");
+    }
+  }
+
+  
+
+  return (
+    <nav className="flex items-center justify-between w-full border-b border-gray-700">
+      <div className="flex items-center justify-start pl-3">
+        <img
+          className="h-10 w-auto"
+          src="/src/assets/logo/png/Small-logo-color.png"
+          alt="Small Logo"
+        />
+        {!isSmallIcon && (
+          <button
+            onClick={() => navigate("/")}
+            className="text-2xl font-bold pl-3 bg-transparent border-none cursor-pointer focus:outline-none hover:text-shadow"
+            style={{ transition: "text-shadow 0.3s" }}
+            onMouseOver={(e) =>
+              (e.currentTarget.style.textShadow = "2px 2px 8px rgba(0,0,0,0.5)")
+            }
+            onMouseOut={(e) => (e.currentTarget.style.textShadow = "none")}
+          >
+            Coinfolio
+          </button>
+        )}
+      </div>
+
+      <div className="flex items-center gap-2">
+        {!isSmallSearch && (
+          <div className="search-bar">
+            <input
+              className="px-3 py-2 border border-gray-300 rounded-md"
+              type="text"
+              placeholder="Search"
+              onChange = {(e)=>{
+                let filteredCoins = coins.filter((coin)=>{
+                  return coin.name.toLowerCase().includes(e.target.value.toLowerCase());
+                });
+                setDisplayedCoins(filteredCoins);
+              }}
+            />
+          </div>
+        )}
+        <div className="buttons pr-1">
+          <button
+            className="px-3 py-2 m-1 bg-blue-700 text-white rounded-md"
+            onClick={() => navigateBasedOnLogging(isLoggedIn)}
+          >
+            Portfolio
+          </button>
+          <button className="px-3 py-2 m-1 bg-blue-700 text-white rounded-md">
+            {signingButtonText}
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Nav;
