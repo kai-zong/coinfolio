5c53f0432bbabdc150653c5ce1ea3847171049ba
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-18 14:28:49 -0700

    Feat: add coin table to database and use prisma seeding to fill coin values

--- coinfolio-project/api/prisma/migrations/20240418195942_make_coind_id_int/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `coinId` on the `Transaction` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
-
-*/
--- AlterTable
-ALTER TABLE `Transaction` MODIFY `coinId` INTEGER NOT NULL;
