41e2a180dd67599052482e3d9da93859e3e61153
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-20 16:37:09 -0700

    Feat: switch to icons for edit & delete button

--- coinfolio-project/src/components/portfolio/transaction/TransDetails.jsx
+++ coinfolio-project/src/components/portfolio/transaction/TransDetails.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function TransDetails({ transaction, index, onTransactionDelete }) {
 
     const formattedDate = new Date(transaction.createdAt).toLocaleDateString();
-    const formattedTime = new Date(transaction.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'}); 
+    const formattedTime = new Date(transaction.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
     const handleDelete = () => {
         if (window.confirm("Are you sure you want to delete this transaction?")) {
@@ -28,7 +28,6 @@ function TransDetails({ transaction, index, onTransactionDelete }) {
 
     return (
         <tr key={transaction.id} className="divide-solid">
-            
 
             <td className="border-t border-b border-gray-700 px-4 py-2">{transaction.id}</td>
             <td className="border-t border-b border-gray-700 px-4 py-2 items-center"> {/* Added 'items-center' class */}
@@ -45,7 +44,18 @@ function TransDetails({ transaction, index, onTransactionDelete }) {
             <td className="border-t border-b border-gray-700 px-4 py-2">{transaction.amount}</td>
             <td className="border-t border-b border-gray-700 px-4 py-2">${transaction.amountInUSD.toFixed(2)}</td>
             <td className="border-t border-b border-gray-700 px-4 py-2">
-                <button onClick={handleDelete}>Delete</button>
+                {/* edit icon     */}
+                <svg class="h-6 w-6" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />  <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />  <line x1="16" y1="5" x2="19" y2="8" /></svg>
+            </td>
+            <td className="border-t border-b border-gray-700 px-4 py-2" onClick={handleDelete}>
+                <svg class="h-6 w-6" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <line x1="4" y1="7" x2="20" y2="7" />
+                    <line x1="10" y1="11" x2="10" y2="17" />
+                    <line x1="14" y1="11" x2="14" y2="17" />
+                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg>
             </td>
         </tr>
     );
--- coinfolio-project/src/components/portfolio/transaction/Transactions.jsx
+++ coinfolio-project/src/components/portfolio/transaction/Transactions.jsx
@@ -32,28 +32,28 @@ function Transactions() {
     return (
         <div className='w-full px-5 m-3'>
             <table className="table-auto w-full text-left">
-            <thead>
-                <tr>
-                    <th className="px-4 py-2">Transaction ID</th>
-                    <th className="px-4 py-2">Logo</th>
-                    <th className="px-4 py-2">Symbol</th>
-                    <th className="px-4 py-2">Coin</th>
-                    <th className="px-4 py-2">Type</th>
-                    <th className="px-4 py-2">Date</th>
-                    <th className="px-4 py-2">Amount</th>
-                    <th className="px-4 py-2">AmountInUSD</th>
-                </tr>
-            </thead>
-            <tbody>
-                {transactions
-                    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-                    .map((transaction, index) => (
-                        <TransDetails key={index} transaction={transaction} onTransactionDelete={handleDeleteTransaction} />
-                ))}
-            </tbody>
-        </table>
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2">Transaction ID</th>
+                        <th className="px-4 py-2">Logo</th>
+                        <th className="px-4 py-2">Symbol</th>
+                        <th className="px-4 py-2">Coin</th>
+                        <th className="px-4 py-2">Type</th>
+                        <th className="px-4 py-2">Date</th>
+                        <th className="px-4 py-2">Amount</th>
+                        <th className="px-4 py-2">AmountInUSD</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {transactions
+                        .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                        .map((transaction, index) => (
+                            <TransDetails key={index} transaction={transaction} onTransactionDelete={handleDeleteTransaction} />
+                        ))}
+                </tbody>
+            </table>
         </div>
-        
+
     );
 }
 
