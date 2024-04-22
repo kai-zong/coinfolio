9eb103a4f3a47e536a664f637ee85d304b1a7a0d
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-22 12:39:50 -0700

    Feat: show diff result in home page for login users

--- coinfolio-project/src/components/home/PriceTable.jsx
+++ coinfolio-project/src/components/home/PriceTable.jsx
@@ -2,29 +2,35 @@ import React from 'react';
 import Ticker from './Ticker';
 import { useState } from 'react';
 import { useUserAndPriceTable } from '../../UserAndPriceTableContext';
-import {useAuth0} from '@auth0/auth0-react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 
 function PriceTable() {
     const { displayedCoins, } = useUserAndPriceTable();
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, user } = useAuth0();
 
-    console.log('displayedCoins:', displayedCoins);
+    console.log('user:', user);
 
     return (
         <div className='w-full px-5 m-3 '>
 
-            <div className='bg-gray-800 rounded-2xl'>
+            {isAuthenticated ? <div className='bg-gray-800 rounded-2xl'>
+                <h1 className="text-4xl font-bold text-center mb-8 pt-3">
+                    Welcome, {user.name}!
+                </h1>
+                <p className="text-lg text-center mb-4 pb-3">
+                    Start tracking your portfolio of Top 50 Crypto Assets
+                </p>
+
+            </div> : <div className='bg-gray-800 rounded-2xl'>
                 <h1 className="text-4xl font-bold text-center mb-8 pt-3">
                     Welcome to Coinfolio!
                 </h1>
-                {isAuthenticated ? <p className="text-lg text-center mb-4 pb-3">
-                    Start tracking your portfolio of Top 50 Crypto Assets
-                </p> : <p className="text-lg text-center mb-4 pb-3">
+                <p className="text-lg text-center mb-4 pb-3">
                     Login to start tracking your portfolio of Top 50 Crypto Assets
-                </p>}
+                </p>
 
-            </div>
+            </div>}
 
             <table className="table-auto w-full text-left">
                 <thead>
