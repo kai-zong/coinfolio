9ffbe9b48f878715143d5d56125e2d0ba390614c
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-04-28 10:55:59 -0700

    Fix: macOS somehow ignore case sensitive, config git ignorecase to false
    https://stackoverflow.com/questions/6899582/i-change-the-capitalization-of-a-directory-and-git-doesnt-seem-to-pick-up-on-it

---
+++ client/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Navigation from './Navigation';
+import CoinMenu from './transaction/CoinMenu';
+import TransactionForm from './transaction/TransactionForm';
+import { useUserAndPriceTable } from '../../UserAndPriceTableContext';
+
+function Portfolio() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isFormOpen, setIsFormOpen] = useState(false);
+    const [selectedCoin, setSelectedCoin] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+    const { displayedCoins } = useUserAndPriceTable();
+
+    const filteredCoins = displayedCoins.filter(coin =>
+        coin.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+    const closeMenu = () => setIsMenuOpen(false);
+    const handleCoinClick = (coin) => {
+        setSelectedCoin(coin);
+        setIsFormOpen(true);
+        setIsMenuOpen(false);
+    };
+    const closeForm = () => {
+        setIsFormOpen(false);
+        setSelectedCoin(null);
+    };
+
+    const menuClasses = `fixed inset-y-0 left-0 transform bg-gray-800 text-white transition duration-300 ease-in-out z-20 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`;
+
+    return (
+        <div className="">
+            <Navigation toggleMenu={toggleMenu} />
+            <div className={menuClasses}>
+                <CoinMenu
+                    searchQuery={searchQuery}
+                    setSearchQuery={setSearchQuery}
+                    filteredCoins={filteredCoins}
+                    handleCoinClick={handleCoinClick}
+                    closeMenu={closeMenu}
+                />
+            </div>
+            <main className="flex-grow">
+                <Outlet />
+            </main>
+            {selectedCoin && (
+                <TransactionForm isOpen={isFormOpen} onClose={closeForm} selectedCoin={selectedCoin} />
+)}
+        </div>
+    );
+}
+
+export default Portfolio;
